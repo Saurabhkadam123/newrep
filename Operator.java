0ea7cc690c81309c68c8a1ac44e9a00b67fb3d5a
Saurabh Rajendra kadam <101618292+Saurabhkadam123@users.noreply.github.com>
2022-03-16 10:24:09 +0530

    one java file uploaded

---
+++ Operator.java
@@ -0,0 +1,18 @@
+package pack1;
+
+public class Operator {
+	void check()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		int marks=35;
+		String result;
+		result=(marks>40) ? "pass" : "fail";
+		System.out.println("result is:"+result);
+		
+
+	}
+
+}
